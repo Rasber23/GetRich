adf46ca5968904c8653ee5f429dff76ddc5f08e1
RasBER <rasmusbernestal@gmail.com>
2021-01-27 19:45:50 +0100

    <feat> User Variabel & User Generic Methods
    User Variabel
    User Generic Methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,74 @@
+import java.util.List;
+import java.util.Objects;
+
 public class Player {
+
+    private String name;
+    private int age;
+    private double balance;
+    private List<Stock> listOfStocks;
+    private double wealth;
+
+    public Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.balance = 0;
+        this.wealth = 0;
+        listOfStocks = new arrayList<Stock>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getWealth() {
+        return wealth;
+    }
+
+    public void setWealth(double wealth) {
+        this.wealth = wealth;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", balance=" + balance +
+                ", wealth=" + wealth +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return age == player.age && name.equals(player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
