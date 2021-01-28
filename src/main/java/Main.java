1047548792406adfe9acf094a9b5e19cea5f1383
Helena <helena.theise@gmail.com>
2021-01-28 21:15:36 +0100

    <feat> TEST BRANCH - Added createNewUser() in Game.java, user can input their name and age in in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,16 @@ public class Main {
         Game game = new Game();
         int userinput = 0;
 
+        System.out.println("What is your name?");
+        String userName = scan.next();
+        System.out.println("How old are you?");
+        int userAge = scan.nextInt();
+
+        /*Testade att göra en branch och la till att man ska kunna skriva in sitt namn och ålder här i main,
+        själva metoden ligger i Game.java och är egentligen bara en lite fancy konstruktor.
+        Feel free to change whatever you want /Helena */
+        game.createNewUser(userName, userAge);
+
         while (userinput != 9) {
             System.out.println("What do you want to do?");
             System.out.println("Press 1 for buy stocks");
