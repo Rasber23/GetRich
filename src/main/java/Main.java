b8e9a92351da76daec494d88cac4f477f08afc17
Helena <helena.theise@gmail.com>
2021-01-29 22:46:43 +0100

    <feat> added player.getBalance() to whatDoYouWant() menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,20 +12,18 @@ public class Main {
         System.out.println("How old are you?");
         int userAge = scan.nextInt();
 
-        /*Testade att göra en branch och la till att man ska kunna skriva in sitt namn och ålder här i main,
-        själva metoden ligger i Game.java och är egentligen bara en lite fancy konstruktor.
-        Feel free to change whatever you want /Helena */
-        game.createNewUser(userName, userAge);
+        Player player = new Player(userName, userAge);
 
         while (userinput != 9) {
             System.out.println("What do you want to do?");
             System.out.println("Press 1 for buy stocks");
             System.out.println("Press 2 sell stocks");
             System.out.println("Press 3 for sleep");
+            System.out.println("Press 4 to see your current balance");
             userinput = scan.nextInt();
 
 
-            game.whatDoYouWant(userinput);
+            game.whatDoYouWant(userinput, player);
         }
     }
 }
