33a4708b27756519146e57d08f7840f012795c9b
Helena <helena.theise@gmail.com>
2021-02-02 22:36:14 +0100

    <feat> try-with-resources and IOException in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,47 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
 
-        Game game = new Game();
-        int userinput = 0;
+        try (Scanner scan = new Scanner(System.in)) {
 
-        System.out.println("What is your name?");
-        String userName = scan.next();
-        System.out.println("How old are you?");
-        int userAge = scan.nextInt();
+            Game game = new Game();
+            int userinput = 0;
 
-        Player player = new Player(userName, userAge);
+            System.out.println("What is your name?");
+            String userName = scan.next();
+            System.out.println("How old are you?");
+            int userAge = scan.nextInt();
 
-        player.calculateWealth();
+            Player player = new Player(userName, userAge);
 
-        while (userinput != 9) {
-            if (game.getRound() > 5) {
-                break;
+            player.calculateWealth();
+
+            while (userinput != 9) {
+                if (game.getRound() > 5) {
+                    break;
+                }
+                System.out.println("Day: " + game.getRound());
+                System.out.println("You are worth: " + player.getWealth());
+                System.out.println("What do you want to do?");
+                System.out.println("Press 1 for buy stocks");
+                System.out.println("Press 2 sell stocks");
+                System.out.println("Press 3 for sleep");
+                System.out.println("Press 4 to see your current balance");
+                userinput = scan.nextInt();
+                game.whatDoYouWant(userinput, player);
             }
-            System.out.println("Day: " + game.getRound());
-            System.out.println("You are worth: " + player.getWealth());
-            System.out.println("What do you want to do?");
-            System.out.println("Press 1 for buy stocks");
-            System.out.println("Press 2 sell stocks");
-            System.out.println("Press 3 for sleep");
-            System.out.println("Press 4 to see your current balance");
-            userinput = scan.nextInt();
-            game.whatDoYouWant(userinput, player);
+
+            if (game.endGame(player)) {
+                System.out.println("Congratz" + player.getName() + ", you got some serious gains!");
+                System.out.println("You started out with 100 and now u got " + player.getWealth() * (1 - Game.tax));
+            } else {
+                System.out.println("GAME OVER, you lived fasted and died young!");
+            }
         }
 
-        if (game.endGame(player)) {
-            System.out.println("Congratz"+ player.getName() + ", you got some serious gains!");
-            System.out.println("You started out with 100 and now u got " + player.getWealth() * (1 - Game.tax));
-        }
 
-        else {
-            System.out.println("GAME OVER, you lived fasted and died young!");
-        }
     }
 }
 
