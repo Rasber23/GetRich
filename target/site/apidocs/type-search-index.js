89b2fd685cde4a9ba0d6d761089c1dc215274468
RasBER <rasmusbernestal@gmail.com>
2021-02-03 11:15:49 +0100

    <fix> Deleted site dir

--- target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"InsufficientBalanceException"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Risk"},{"p":"<Unnamed>","l":"Stock"}]
