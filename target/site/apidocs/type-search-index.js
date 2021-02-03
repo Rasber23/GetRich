a55f8b25638449144ca6c1cf3773b4aae0d5f0fb
RasBER <rasmusbernestal@gmail.com>
2021-02-03 10:34:23 +0100

    added javaDoc

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"InsufficientBalanceException"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Risk"},{"p":"<Unnamed>","l":"Stock"}]
